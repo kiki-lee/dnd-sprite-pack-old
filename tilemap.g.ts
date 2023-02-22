3bdbb57b3bbbe5b3683006a92d3fb2c1bbbac5b9
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2023-02-22 13:53:08 -0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,11 +1,11 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
+    export const transparency18 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "transparency16":return transparency16;
+            case "transparency18":return transparency18;
         }
         return null;
     })
